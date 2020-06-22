f5ce9467fb22174c7e929fe299796cd6e3656d45
Umair Anhar <umair.anhar@outlook.com>
2020-06-22 18:57:38 +0530

    Connection String

---
+++ MicroRabbit/MicroRabbit.Banking.Data/Repository/AccountRepository.cs
@@ -0,0 +1,25 @@
+﻿using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Banking.Data.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private BankingDbContext context;
+        public AccountRepository(BankingDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<IEnumerable<Account>> GetAccounts()
+        {
+            return await context.Accounts.ToListAsync();
+        }
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Banking.Domain/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Banking.Domain.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
