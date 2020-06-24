7c02096a083024cf66aad6235b1600c7c7292f15
Raees Samman <sammanmra008@gmail.com>
2020-06-24 12:19:50 +0530

    Implemented Publish and SendCommand for Banking Microservices

--- MicroRabbit/MicroRabbit.Banking.Application/Services/AccountService.cs
+++ MicroRabbit/MicroRabbit.Banking.Application/Services/AccountService.cs
@@ -1,6 +1,9 @@
 ﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Models;
+using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Banking.Domain.Models;
+using MicroRabbit.Domain.Core.Bus;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,14 +15,23 @@ namespace MicroRabbit.Banking.Application.Services
     public class AccountService : IAccountService
     {
         private readonly IAccountRepository accountRepository;
-        public AccountService(IAccountRepository accountRepository)
+        private readonly IEventBus eventBus;
+        public AccountService(IAccountRepository accountRepository, IEventBus eventBus)
         {
             this.accountRepository = accountRepository;
+            this.eventBus = eventBus;
         }
 
         public IEnumerable<Account> GetAccounts()
         {
             return accountRepository.GetAccounts();
+        }
+
+        public void Transfer(AccountTransfer accountTransfer)
+        {
+            var createTransferCommand = new CreateTransferCommand(accountTransfer.FromAccount, accountTransfer.ToAccount, accountTransfer.TransferAmount);
+
+            eventBus.SendCommand(createTransferCommand);
         }
     }
 }
---
+++ MicroRabbit/MicroRabbit.Banking.Domain/CommandHandlers/TransferCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using MicroRabbit.Banking.Domain.Commands;
+using MicroRabbit.Banking.Domain.Events;
+using MicroRabbit.Domain.Core.Bus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Banking.Domain.CommandHandlers
+{
+    public class TransferCommandHandler : IRequestHandler<CreateTransferCommand, bool>
+    {
+        private readonly IEventBus eventBus;
+
+        public TransferCommandHandler(IEventBus eventBus)
+        {
+            this.eventBus = eventBus;
+        }
+
+        public Task<bool> Handle(CreateTransferCommand request, CancellationToken cancellationToken)
+        {
+            //publish event to RabbitMQ
+            eventBus.Publish(new TransferCreatedEvent(request.From, request.To, request.Amount));
+            return Task.FromResult(true);
+        }
+    }
+}
